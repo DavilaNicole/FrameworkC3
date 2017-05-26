2f2e62afb8b929c380e69b30eca74bcc9f647790
DavilaNicole <nicoledavilaa@gmail.com>
2017-05-26 06:36:03 -0300

    v.1

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+
+	    <meta charset="utf-8">
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    
+	    <title>C3 Framework</title>
+
+	    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+	    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+	    <link rel="stylesheet" href="css/framework.css">
+        <link rel="stylesheet" href="bootstrap/css/font-awesome.min.css">
+
+	</head>
+	
+	<body>
+    <!-- Fixed navbar -->
+	    <div class="navbar navbar-inverse navbar-fixed-top">
+    		<div class="container">
+        		<div class="navbar-header">
+          			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            			<span class="icon-bar"></span>
+            			<span class="icon-bar"></span>
+         			</button>
+          			<a class="navbar-brand" href="index.php">C3 Framework</a>
+        		</div>
+        		<div class="navbar-collapse collapse">
+          			<ul class="nav navbar-nav navbar-right">
+            			<li class="active"><a href="index.php">Principal</a></li>
+            			<li><a href="framework.php">Framework</a></li>
+          			</ul>
+        		</div>
+      		</div>
+    	</div>
+    	<!-- Header -->
+		<div id="headerindex">
+			<div class="container">
+				<div class="row centered">
+					<div class="col-lg-12">
+						<h1>C3 - <i>Framework</i> para Construção Científica na Ciência da Computação</h1>
+						<h2>Apoio à condução de pesquisas científicas na Ciência da Computação</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+    	<!-- Opções página inicial -->
+		<div class="container opcoes">
+			<div class="row centered">
+				<br><br>
+				<div class="col-lg-4">
+					<i class="fa fa-calendar"></i>
+					<h4>GESTÃO</h4>
+					<p>O C3 Framework tem como propósito apoiar a condução da construção científica a todos os tipos de investigação</p>	
+				</div>
+				<div class="col-lg-4">
+					<i class="fa fa-check-circle"></i>
+					<h4>RIGOR</h4>
+					<p>Uma das características é o destaque dado ao rigor científico, incentivamento uma construção sistematizada da pesquisa</p>
+				</div>
+				<div class="col-lg-4">
+					<i class="fa fa-gears"></i>
+					<h4>PRÁTICA E TEORIA</h4>
+					<p>Assim como na Ciência da Computação, no C3 Framework a prática e a teorica coexistem na construção científica</p>
+				</div>
+			</div>
+		</div>	
+		<!-- Informações sobre o framework -->
+		<div id="sobre">
+			<div class="container">
+				<div class="row centered">
+					<br><br>
+					<h4>SOBRE A PROPOSTA</h4>
+					<br>
+					<p>O C3 Framework é um artefato de apoio a condução da pesquisa científica. Sua construção utilizou como referência o cenário da Ciência da Computação, no qual teoria e prática articulam-se para gerar estudo científicos com rigor, relevância e visando soluções para problmemas práticos.</p>
+					<br>
+					<p>A motivação para construir o framework parte da necessidade de mitigar pesquisas na Ciência da Computação que representam apenas apresentações tecnológicas, sem o caráter científico e contribuições para as bases de conhecimento.</p>
+					<br>
+					<p>Esta proposta foi elaborada por Nicole Davila, com orientação da Prof.Ms. Adriana dos Reis. Sua construção é resultado do Trabalho de Conclusão de Curso realizado pelas autores na Universidade Feevale, em Novo Hamburgo/RS.</p>
+				</div>
+			</div>	
+		</div>
+
+		<!-- Conheça o C3 -->
+		<div id="conheca">
+			<div class="container">
+				<div class="row centered">
+					<div class="col-lg-8 col-lg-offset-2">
+						<h4>CONHEÇA O C3 FRAMEWORK</h4>
+						<p>Conheça a proposta e comece a utilizá-la em sua próxima pesquisa científica</p> 
+						<br>
+						<p> <a href="framework.php">Visão Geral</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Contato -->
+		<div id="contato">
+			<div class="container">
+				<div class="row centered">
+	                <i class="fa fa-envelope-o fa-3x sr-contact"></i>
+    	            <p>
+        	            <a href="mailto:nicoledavilaa@gmail.com">nicoledavilaa@gmail.com</a>
+            	    </p>
+				</div>
+			</div>
+		</div>
+
+    	<!-- jQuery (obrigatório para plugins JavaScript do Bootstrap) -->
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    	<!-- Inclui todos os plugins compilados (abaixo), ou inclua arquivos separadados se necessário -->
+	    <script src="bootstrap/js/bootstrap.js"></script>
+	</body>
+</html>
+
